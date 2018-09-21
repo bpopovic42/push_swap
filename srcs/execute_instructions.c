75b3a9fdb75f3eba65dc7e0bfc9bfbb90839fac0
bpopov <byurinov1@gmail.com>
2018-09-21 23:31:28 +0200

    [MOD][WIP] Now execute functions from registered function pointers of the instruction list

--- srcs/execute_instructions.c
+++ srcs/execute_instructions.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 19:05:19 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/21 13:11:00 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/21 21:48:16 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,16 @@ int		execute_instructions(t_stacks *stacks, t_list **inst, t_flags *flags)
 	(void)stacks;
 	(void)inst;
 	(void)flags;
+	t_list	*ptr;
+	ptr = *inst;
+	while (ptr)
+	{
+		ft_putstr((char*)((t_inst*)ptr->content)->name);
+		ft_putchar('\n');
+		((void)((t_inst*)ptr->content)->function(stacks));
+		print_stack(stacks->head_a, 'A');
+		print_stack(stacks->head_b, 'B');
+		ptr = ptr->next;
+	}
 	return (0);
 }
