11dd73820d403c2148647525644184c673ae4a48
bpopov <byurinov1@gmail.com>
2018-09-21 23:30:18 +0200

    [FIX] Now correctly frees allocated structures by passing correct function to ft_strdel and taking care of pointing the final list link's NEXT pointer to null

--- srcs/checker.c
+++ srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 18:21:26 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/21 16:28:15 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/21 20:11:04 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,12 @@
 
 static void		free_structures(t_stacks *stacks, t_list **inst)
 {
-	ft_lstdel(inst, ft_bzero);
+	ft_lstdel(inst, &del_instruction);
+	if (stacks->head_a && stacks->head_a->prev)
+		stacks->head_a->prev->next = NULL;
 	ft_dlstdel(&(stacks->head_a), ft_bzero);
+	if (stacks->head_b && stacks->head_b->prev)
+		stacks->head_b->prev->next = NULL;
 	ft_dlstdel(&(stacks->head_b), ft_bzero);
 }
 
@@ -66,14 +70,6 @@ int		main(int ac, char **av)
 	if (get_instructions(&instructions) < 0)
 		return (clean_exit("Bad instruction", &stacks, &instructions));
 	execute_instructions(&stacks, &instructions, &flags);
-	print_stack(stacks.head_a, 'A');
-	print_stack(stacks.head_b, 'B');
-
-	ft_putstr("[SWAP A]\n");
-	swap_a(&stacks);
-	print_stack(stacks.head_a, 'A');
-	print_stack(stacks.head_b, 'B');
-
 	//check_if_sorted(head_a, head_b);
 	//print_instructions(&instructions);
 	free_structures(&stacks, &instructions);
