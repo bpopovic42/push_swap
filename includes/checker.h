45ec8200e838bbdb89e2e032cc399bd1c7323b61
bpopov <byurinov1@gmail.com>
2018-09-21 23:19:32 +0200

    [ADD][MOD] Added functions, renamed some parameters

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/12 17:25:30 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/21 13:26:03 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/21 23:00:04 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,11 @@ typedef struct		s_stacks
 	t_dlist			*head_b;
 }					t_stacks;
 
-typedef struct		s_instruction
+typedef struct		s_inst
 {
 	void			(*function)(t_stacks*);
 	char			*name;
-}					t_instruction;
+}					t_inst;
 
 /*
 ** FUNCTIONS
@@ -49,11 +49,24 @@ int			put_error(char *msg, int return_value);
 t_dlist		*get_next_val_if_valid(char *value);
 int			check_characters(char *str);
 int			get_input(int ac, char **av, t_dlist **head_a, t_flags *flags);
-int			get_instructions(t_list **instructions);
+int			get_instructions(t_list **inst);
 int			execute_instructions(t_stacks *stacks, t_list **inst, t_flags *flags);
-int			swap_a(t_stacks *stacks);
-int			swap_b(t_stacks *stacks);
-int			push_a(t_stacks *stacks);
-int			push_b(t_stacks *stacks);
+void		swap_a(t_stacks *stacks);
+void		swap_b(t_stacks *stacks);
+void		swap_ab(t_stacks *stacks);
+void		push_a(t_stacks *stacks);
+void		push_b(t_stacks *stacks);
+void		rotate_a(t_stacks *stacks);
+void		rotate_b(t_stacks *stacks);
+void		rotate_ab(t_stacks *stacks);
+void		reverse_rotate_a(t_stacks *stacks);
+void		reverse_rotate_b(t_stacks *stacks);
+void		reverse_rotate_ab(t_stacks *stacks);
+void		del_instruction(void *inst, size_t size);
+
+/*
+** DEBUGGING
+*/
+void		print_link_state(t_dlist *link);
 
 #endif
