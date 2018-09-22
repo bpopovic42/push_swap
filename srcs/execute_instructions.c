b046e6cd5918cad735a4669878ef48de966f6707
bpopov <byurinov1@gmail.com>
2018-09-22 11:00:40 +0200

    [MOD] Now only prints something if flag '-v' is active

--- srcs/execute_instructions.c
+++ srcs/execute_instructions.c
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/18 19:05:19 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/21 21:48:16 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/09/22 10:56:58 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,22 @@
 
 int		execute_instructions(t_stacks *stacks, t_list **inst, t_flags *flags)
 {
-	(void)stacks;
-	(void)inst;
 	(void)flags;
 	t_list	*ptr;
 	ptr = *inst;
 	while (ptr)
 	{
-		ft_putstr((char*)((t_inst*)ptr->content)->name);
-		ft_putchar('\n');
+		if (flags->visualizer)
+		{
+			ft_putstr((char*)((t_inst*)ptr->content)->name);
+			ft_putchar('\n');
+		}
 		((void)((t_inst*)ptr->content)->function(stacks));
-		print_stack(stacks->head_a, 'A');
-		print_stack(stacks->head_b, 'B');
+		if (flags->visualizer)
+		{
+			print_stack(stacks->head_a, 'A');
+			print_stack(stacks->head_b, 'B');
+		}
 		ptr = ptr->next;
 	}
 	return (0);
