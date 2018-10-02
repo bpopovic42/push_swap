643ab5a57f7f6d65ef2fea8a75c35abd2b06488b
Boris POPOVIC <bopopovi@student.42.fr>
2018-10-02 19:27:37 +0200

    [MOD] Subsequent changes to push_swap's header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bopopovi <bopopovi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/22 22:12:30 by bopopovi          #+#    #+#             */
-/*   Updated: 2018/09/23 16:21:47 by bopopovi         ###   ########.fr       */
+/*   Updated: 2018/10/02 18:31:31 by bopopovi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 ** FUNCTIONS
 */
 
-int			get_median(t_stacks *stacks);
-void		sort_stacks(t_stacks *stacks, t_list **inst, int median);
+int			get_median(t_dlist *head);
+void		sort_stacks(t_stacks *stacks, t_list **inst);
+int			get_list_len(t_dlist *head);
 
 #endif
